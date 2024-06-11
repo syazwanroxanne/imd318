6f08e708fb13059f26aba1f0199b9d9090e40ecd
syazwan <syazwanmohideen@gmail.com>
2024-06-11 20:45:53 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const dropdown = document.querySelector('.dropdown');
+    const dropdownContent = document.querySelector('.dropdown-content');
+    
+    dropdown.addEventListener('mouseover', () => {
+        dropdownContent.style.display = 'block';
+    });
+
+    dropdown.addEventListener('mouseout', () => {
+        dropdownContent.style.display = 'none';
+    });
+});
